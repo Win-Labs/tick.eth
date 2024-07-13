0466fa29403851f0e25e939e41f3d584007e5267
gylman <gylmanbm@gmail.com>
2024-07-13 22:15:04 +0900

    .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const HOLESKY_RPC_URL = process.env.HOLESKY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    holesky: {
+      url: HOLESKY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 17000,
+    },
+  },
 };
