43055d39c6995b4a34da65c2a2548e0c4090b490
gylman <gylmanbm@gmail.com>
2024-07-14 00:56:21 +0900

    extract ens info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import ConnectButton from "./components/ConnectButton";
-import { useAccount, useEnsAddress, useEnsAvatar, useEnsName } from "wagmi";
+import {
+  useAccount,
+  useEnsAddress,
+  useEnsAvatar,
+  useEnsName,
+  useEnsText,
+} from "wagmi";
 import { normalize } from "viem/ens";
 import { formatAddress } from "@ens-tools/format";
 
 function App() {
-  const name = normalize("luc.eth");
-  const { data: avatar } = useEnsAvatar({ name });
+  const name = normalize("ethbrussels.eth");
+  const { data: avatar } = useEnsAvatar({ name: name, chainId: 17000 });
   const { data: ethereum } = useEnsAddress({ name, coinType: 60 });
+
+  const { data } = useEnsText({
+    name: normalize("luc.eth"),
+    key: "com.twitter",
+  });
 
   return (
     <>
@@ -16,6 +27,10 @@ function App() {
         {ethereum && formatAddress(ethereum)}
         <br />
         {avatar && <img src={avatar} />}
+        <div>
+          <span>Twitter: </span>
+          <span>{data}</span>
+        </div>
       </div>
       <ConnectButton />
     </>
